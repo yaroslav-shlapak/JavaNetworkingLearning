85bf41e0adcd6366b1d6623fd6ecbc4bae7b01e6
y.shlapak <y.shlapak@samsung.com>
2015-02-04 19:41:03 +0200

    Modifiacation of Client and Server classes for modularity Start adding unit tests
    Signed-off-by: y.shlapak <y.shlapak@samsung.com>

--- src/tcpclientserver/Client.java
+++ src/tcpclientserver/Client.java
@@ -6,39 +6,74 @@ package tcpclientserver;
 
 import com.google.gson.Gson;
 
-import java.io.*;
-import java.net.InetSocketAddress;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
 
+    public Client(String hostName, int portNumber) {
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+    }
 
-        final String hostName = "localhost";
-        final int portNumber = 4446;
-        final SocketAddress address = new InetSocketAddress(hostName, portNumber);
-        Thread thread = new Thread() {
+    final String hostName;
+    final int portNumber;
+
+    public LightControlJson getLightControlJson() {
+        return lightControlJson;
+    }
+
+    public void setLightControlJson(LightControlJson lightControlJson) {
+        this.lightControlJson = lightControlJson;
+    }
+
+    private LightControlJson lightControlJson;
+
+    public void send(LightControlJson lightControlJsonIn) {
+        lightControlJson = lightControlJsonIn;
+
+        new Thread() {
             @Override
             public void run() {
-
-
                 try (
-                        Socket socket = new Socket();
-                        DataInputStream fromServer = new DataInputStream(socket.getInputStream());
+                        Socket socket = new Socket(hostName, portNumber);
                         DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
                 ) {
-                    String toServerString, fromServerString;
-                    socket.connect(address);
-
-                    LightControlJson lightControlJson = new LightControlJson(true, 20);
+                    String toServerString;
                     Gson json = new Gson();
                     toServerString = json.toJson(lightControlJson);
                     toServer.writeUTF(toServerString);
 
-                    socket.close();
+                } catch (UnknownHostException e) {
+                    System.err.println("Don't know about host " + hostName);
+                    System.exit(1);
+                } catch (IOException e) {
+                    System.err.println("Couldn't get I/O for the connection to " +
+                            hostName);
+                    System.exit(1);
+                }
+            }
+        }.start();
 
+    }
+
+    public LightControlJson receive() {
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try (
+                        Socket socket = new Socket(hostName, portNumber);
+                        DataInputStream fromServer = new DataInputStream(socket.getInputStream());
+                ) {
+                    String fromServerString;
+                    Gson json = new Gson();
+
+                    fromServerString = fromServer.readUTF();
+                    lightControlJson = json.fromJson(fromServerString, LightControlJson.class);
                 } catch (UnknownHostException e) {
                     System.err.println("Don't know about host " + hostName);
                     System.exit(1);
@@ -50,6 +85,13 @@ public class Client {
             }
         };
         thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return lightControlJson;
 
     }
 }
---
+++ src/tcpclientserver/Main.java
@@ -0,0 +1,21 @@
+package tcpclientserver;
+
+import java.io.IOException;
+
+/**
+ * Created by y.shlapak on Feb 04, 2015.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String hostName = "localhost";
+        int port = 4444;
+        Client client = new Client(hostName, port);
+        Server server = new Server(port);
+
+        server.send();
+        client.send();
+        client.receive();
+
+    }
+}
