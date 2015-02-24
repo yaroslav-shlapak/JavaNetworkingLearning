3a3fea54068ba46db0b3913d7223b236d71be906
Yaroslav <yarosalv.shlapak@gmail.com>
2015-02-25 00:41:55 +0200

    fix

--- src/tcpclientserver/Main.java
+++ src/tcpclientserver/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
         String hostName = "192.168.0.103";
         int port = 1234;
         Server server = new Server(port);
@@ -18,31 +17,7 @@ public class Main {
             server.receive();
             System.out.println(server.getLightControlJson().getLightLevel());
             System.out.println(server.getLightControlJson().isLightEnabled());
-=======
-        boolean defBool1 = true;
-        boolean defBool2 = false;
 
-        int defInt1 = 20;
-        int defInt2 = 50;
-
-        String hostName = "localhost";
-        int port = 4444;
-        Client client = new Client(hostName, port);
-        Server server = new Server(port);
-
-        LightControlJson lightControlJson = new LightControlJson(defBool1, defInt1);
-
-        while(true) {
-            client.send(lightControlJson);
-            lightControlJson = server.receive();
-            lightControlJson.setLightLevel(lightControlJson.getLightLevel() + 1);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(lightControlJson.getLightLevel());
->>>>>>> 57c1f8949bc771384226d469ed3c8048eaee29b8
         }
 
     }
